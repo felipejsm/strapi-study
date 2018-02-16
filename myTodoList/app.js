66cd3f9462d43aa150792eb2ed8eda7b453f5846
Felipe Moysés <felipe.moyses@techne.com.br>
2018-02-16 14:58:39 -0200

    Added fontawesome and remove action

--- myTodoList/app.js
+++ myTodoList/app.js
@@ -3,11 +3,13 @@ function myCtrl() {
     var vm = this;
     vm.myToDoListObj = [];
     vm.addTodoItem = addTodoItem;
-
+    vm.removeTodoItem = removeTodoItem;
     function addTodoItem() {
         vm.myToDoListObj.push({isChecked : false, toDoName: ""});
     }
-
+    function removeTodoItem(index) {
+        vm.myToDoListObj.splice(index, 1);
+    }
     addTodoItem();
 
 };
