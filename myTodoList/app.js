34ab9043c35825a607d4c827ea800144d0ba781c
Felipe Moysés <felipe.moyses@techne.com.br>
2018-02-16 16:43:28 -0200

    Trying to GET or POST - not working... | https://github.com/strapi/strapi/issues/368

--- myTodoList/app.js
+++ myTodoList/app.js
@@ -1,15 +1,42 @@
 angular.module('myApp',[]).controller('myCtrl', myCtrl)
-function myCtrl() {
+function myCtrl($http) {
     var vm = this;
     vm.myToDoListObj = [];
     vm.addTodoItem = addTodoItem;
     vm.removeTodoItem = removeTodoItem;
+    vm.save = save;
+    vm.initial = initial;
+    function initial() {
+        $http({
+            method : "GET",
+            url : "http://localhost:1337/mytodolist"
+        }).then(function mySuccess(response) {
+            if(response.data.length)
+                vm.myToDoListObj = response.data;
+            else
+                addTodoItem();
+        }, function myError(response) {
+            addTodoItem();
+        });
+    }
     function addTodoItem() {
-        vm.myToDoListObj.push({isChecked : false, toDoName: ""});
+        vm.myToDoListObj.push({id: null, isChecked : false, toDoName: ""});
     }
     function removeTodoItem(index) {
-        vm.myToDoListObj.splice(index, 1);
+        if(vm.myToDoListObj.length > 1)
+            vm.myToDoListObj.splice(index, 1);
     }
-    addTodoItem();
+    function save() {
+        $http({
+            method : "POST",
+            url : "http://localhost:1337/mytodolist",
+            data : vm.myToDoListObj
+        }).then(function mySuccess(response) {
+           console.log(response);
+        }, function myError(response) {
+            console.log(response);
+        });
+    }
+    initial();
 
 };
